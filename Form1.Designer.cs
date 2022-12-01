bc8ca738601efeb441ca952351bce6950312499b
345qwr345 <117391136+345qwr345@users.noreply.github.com>
2022-12-01 22:59:50 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+using System;
+
+namespace Deepak_DotNetTask2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textEmployeeName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.EmployeeRecordDataGridView = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtReportTo = new System.Windows.Forms.TextBox();
+            this.txtResignedDate = new System.Windows.Forms.TextBox();
+            this.txtResigned = new System.Windows.Forms.TextBox();
+            this.txtDOJ = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeRecordDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textEmployeeName
+            // 
+            this.textEmployeeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEmployeeName.Location = new System.Drawing.Point(105, 25);
+            this.textEmployeeName.Name = "textEmployeeName";
+            this.textEmployeeName.Size = new System.Drawing.Size(182, 26);
+            this.textEmployeeName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(189, 309);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // EmployeeRecordDataGridView
+            // 
+            this.EmployeeRecordDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployeeRecordDataGridView.Location = new System.Drawing.Point(12, 355);
+            this.EmployeeRecordDataGridView.Name = "EmployeeRecordDataGridView";
+            this.EmployeeRecordDataGridView.RowHeadersWidth = 51;
+            this.EmployeeRecordDataGridView.RowTemplate.Height = 24;
+            this.EmployeeRecordDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.EmployeeRecordDataGridView.Size = new System.Drawing.Size(886, 150);
+            this.EmployeeRecordDataGridView.TabIndex = 3;
+            this.EmployeeRecordDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.EmployeeRecordDataGridView_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(341, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(504, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartment.Location = new System.Drawing.Point(105, 227);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(182, 26);
+            this.txtDepartment.TabIndex = 6;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContact.Location = new System.Drawing.Point(606, 92);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(182, 26);
+            this.txtContact.TabIndex = 8;
+            // 
+            // txtReportTo
+            // 
+            this.txtReportTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReportTo.Location = new System.Drawing.Point(606, 25);
+            this.txtReportTo.Name = "txtReportTo";
+            this.txtReportTo.Size = new System.Drawing.Size(182, 26);
+            this.txtReportTo.TabIndex = 9;
+            // 
+            // txtResignedDate
+            // 
+            this.txtResignedDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResignedDate.Location = new System.Drawing.Point(606, 227);
+            this.txtResignedDate.Name = "txtResignedDate";
+            this.txtResignedDate.Size = new System.Drawing.Size(182, 26);
+            this.txtResignedDate.TabIndex = 10;
+            // 
+            // txtResigned
+            // 
+            this.txtResigned.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResigned.Location = new System.Drawing.Point(606, 159);
+            this.txtResigned.Name = "txtResigned";
+            this.txtResigned.Size = new System.Drawing.Size(182, 26);
+            this.txtResigned.TabIndex = 11;
+            // 
+            // txtDOJ
+            // 
+            this.txtDOJ.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDOJ.Location = new System.Drawing.Point(105, 159);
+            this.txtDOJ.Name = "txtDOJ";
+            this.txtDOJ.Size = new System.Drawing.Size(182, 26);
+            this.txtDOJ.TabIndex = 12;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDOB.Location = new System.Drawing.Point(105, 92);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(182, 26);
+            this.txtDOB.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "DOB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "DOJ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(2, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Department";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(500, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Report To";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(500, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Contact No";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(500, 165);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Resigned";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(485, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 20);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "ResignedDate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(910, 517);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtDOJ);
+            this.Controls.Add(this.txtResigned);
+            this.Controls.Add(this.txtResignedDate);
+            this.Controls.Add(this.txtReportTo);
+            this.Controls.Add(this.txtContact);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.EmployeeRecordDataGridView);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textEmployeeName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeRecordDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textEmployeeName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView EmployeeRecordDataGridView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtReportTo;
+        private System.Windows.Forms.TextBox txtResignedDate;
+        private System.Windows.Forms.TextBox txtResigned;
+        private System.Windows.Forms.TextBox txtDOJ;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Deepak_DotNetTask2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=db_Employees;Integrated Security=True");
+        public int EmployeeId;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetEmployeeRecords();
+        }
+
+        private void GetEmployeeRecords()
+        {
+            
+            SqlCommand cmd = new SqlCommand("select * from Employees", con);
+            DataTable dt = new DataTable();
+
+            con.Open();
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+
+            EmployeeRecordDataGridView.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(IsValid())
+            {
+                SqlCommand cmd = new SqlCommand("Insert into Employees (EmpName, Dob, Doj, Department, ReportTo, ContactNo, ResignedDate) VALUES (@EmpName,@Dob,@Doj,@Department,@ReportTo,@ContactNo,@ResignedDate)",con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@EmpName", textEmployeeName.Text);
+                cmd.Parameters.AddWithValue("@Dob", Convert.ToDateTime(txtDOB.Text));
+                cmd.Parameters.AddWithValue("@Doj", Convert.ToDateTime(txtDOJ.Text));
+                cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+                cmd.Parameters.AddWithValue("@ReportTo", txtReportTo.Text);
+                cmd.Parameters.AddWithValue("@ContactNo", txtContact.Text);
+                cmd.Parameters.AddWithValue("@ResignedDate", Convert.ToDateTime(txtResignedDate.Text));
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("New Employee is successfuly saved","Saved",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                GetEmployeeRecords();
+            }
+        }
+
+        private bool IsValid()
+        {
+            if(textEmployeeName.Text == string.Empty)
+            {
+                MessageBox.Show("Employee name is required","Failed",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void EmployeeRecordDataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            EmployeeId = Convert.ToInt32(EmployeeRecordDataGridView.SelectedRows[0].Cells[0].Value);
+            textEmployeeName.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[1].Value.ToString();
+            txtDOB.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[2].Value.ToString();
+            txtDOJ.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[3].Value.ToString();
+            txtDepartment.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[4].Value.ToString();
+            txtReportTo.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[5].Value.ToString();
+            txtContact.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[6].Value.ToString();
+            txtResignedDate.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[7].Value.ToString();
+            //textEmployeeName.Text = EmployeeRecordDataGridView.SelectedRows[0].Cells[1].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (EmployeeId>0)
+            {
+                SqlCommand cmd = new SqlCommand("Update Employees set EmpName=@EmpName,Dob=@Dob,Doj=@Doj,Department=@Department,ReportTo=@ReportTo,ContactNo=@ContactNo,ResignedDate=@ResignedDate where EmpCode = @Id", con);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@EmpName", textEmployeeName.Text);
+                cmd.Parameters.AddWithValue("@Dob", Convert.ToDateTime(txtDOB.Text));
+                cmd.Parameters.AddWithValue("@Doj", Convert.ToDateTime(txtDOJ.Text));
+                cmd.Parameters.AddWithValue("@Department", txtDepartment.Text);
+                cmd.Parameters.AddWithValue("@ReportTo", txtReportTo.Text);
+                cmd.Parameters.AddWithValue("@ContactNo", txtContact.Text);
+                cmd.Parameters.AddWithValue("@ResignedDate", Convert.ToDateTime(txtResignedDate.Text));
+                cmd.Parameters.AddWithValue("@Id", this.EmployeeId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Employee Information is updated successfuly", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetEmployeeRecords();
+            }
+            else
+            {
+                MessageBox.Show("Please select employee to update", "Select ?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (EmployeeId>0)
+            {
+                SqlCommand cmd = new SqlCommand("Delete from Employees where EmpCode = @Id", con);
+                cmd.CommandType = CommandType.Text;               
+                cmd.Parameters.AddWithValue("@Id", this.EmployeeId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+
+                MessageBox.Show("Employee Information is deleted successfuly", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                GetEmployeeRecords();
+            }
+            else
+            {
+                MessageBox.Show("Please select employee to delete", "Select ?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
